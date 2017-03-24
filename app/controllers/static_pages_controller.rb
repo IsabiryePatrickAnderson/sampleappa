ae705d610ca96dd44a6422e7e48008c20df962ea
PatrickAnderson <patrickisabirye71@gmail.com>
2017-03-24 12:32:59 +0300

    Finished static pages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,17 @@
 class StaticPagesController < ApplicationController
   def home
+  	render view: "home"
   end
 
   def help
+  	render view: "help"
+  end
+
+  def about
+  	render view: "about"
+  end
+
+  def contact
+  	render view: "contact"
   end
 end
