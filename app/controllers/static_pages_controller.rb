0f336541e7f7516dcc8036fcb0fabb3551f43b23
PatrickAnderson <patrickisabirye71@gmail.com>
2017-04-06 13:25:47 +0300

    railsFlavoured branch

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,17 +1,13 @@
 class StaticPagesController < ApplicationController
   def home
-  	render view: "home"
   end
 
   def help
-  	render view: "help"
   end
 
   def about
-  	render view: "about"
   end
 
   def contact
-  	render view: "contact"
   end
 end
--- config/application.rb
+++ config/application.rb
@@ -7,7 +7,8 @@ require 'rails/all'
 Bundler.require(*Rails.groups)
 
 module Sampleapp
-  class Application < Rails::Application
+  class Application < Rails::
+  	config.autoload_paths += %W(#{config.root}/lib)
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
