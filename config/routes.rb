58955a5ca0184fbdb7aee0a25372df1801c3b6a0
IsabiryePatrickAnderson <patrickisabirye71@gmail.com>
2017-04-11 07:39:25 +0300

    Added booststrap successfully

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,13 @@ Rails.application.routes.draw do
 
   root 'static_pages#home' 
 
-  match '/home', to: 'static_pages#home'
+  get '/home', to: 'static_pages#home'
 
-  match '/help', to: 'static_pages#help'
+  get '/help', to: 'static_pages#help'
 
-  match '/about', to: 'static_pages#about'
+  get '/about', to: 'static_pages#about'
 
-  match '/contact', to: 'static_pages#contact'
+  get '/contact', to: 'static_pages#contact'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
