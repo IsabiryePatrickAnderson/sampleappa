dc8933559f6f778f85df0d6fc7770ee444f9957a
IsabiryePatrickAnderson <patrickisabirye71@gmail.com>
2017-04-11 07:16:51 +0300

    Initial layout filling

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,13 @@ Rails.application.routes.draw do
 
   root 'static_pages#home' 
 
-  get 'static_pages/home', to: 'static_pages#home'
+  match '/home', to: 'static_pages#home'
 
-  get 'static_pages/help', to: 'static_pages#help'
+  match '/help', to: 'static_pages#help'
 
-  get 'static_pages/about', to: 'static_pages#about'
+  match '/about', to: 'static_pages#about'
 
-  get 'static_pages/contact', to: 'static_pages#contact'
+  match '/contact', to: 'static_pages#contact'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
